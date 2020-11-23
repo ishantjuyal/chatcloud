02db2e36533c32b9265fa3390ac3a18d9c4245d8
Ishant Juyal <ishanjj61@gmail.com>
2020-11-23 15:42:57 +0530

    Added version 0.0.4

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='chatclouds',
+  version='0.0.4',
+  description='Allows you to make wordclouds for your Whatsapp Chats',
+  long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='https://github.com/ishantjuyal/chatcloud',  
+  author='Ishant Juyal',
+  author_email='ishantnit@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='wordcloud', 
+  packages=find_packages(),
+  install_requires=['wordcloud', 'matplotlib', 'pandas', 'contractions'] 
+)
